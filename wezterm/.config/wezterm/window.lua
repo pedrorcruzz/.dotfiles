5e4fba8cde74971ef88e7a02e1ff4de99f3c1411
pedrorcruzz <phrosa.dev@gmail.com>
2025-02-12 18:55:37 -0300

    feat: add initial rows and cols

--- wezterm/.config/wezterm/window.lua
+++ wezterm/.config/wezterm/window.lua
@@ -4,6 +4,8 @@ function module.apply_to_config(config)
 	config.window_decorations = "RESIZE"
 	config.window_background_opacity = 0.90
 	config.macos_window_background_blur = 35
+	config.initial_rows = 35
+	config.initial_cols = 120
 	config.window_padding = {
 		left = 0, --16
 		right = 0, --16
