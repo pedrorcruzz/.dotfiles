007fc7ffa02f71bd96f21871946258b063eb6a47
pedrorcruzz <phrosa.dev@gmail.com>
2025-02-16 15:58:18 -0300

    refactor: adjust for biomejs, eslint_d and eslint

--- nvim/.config/nvim/lua/custom/plugins/env/lint.lua
+++ nvim/.config/nvim/lua/custom/plugins/env/lint.lua
@@ -1,59 +1,42 @@
 return {
+  'mfussenegger/nvim-lint',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    local lint = require 'lint'
 
-  { -- Linting
-    "mfussenegger/nvim-lint",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      local lint = require("lint")
-      lint.linters_by_ft = {
-        markdown = { "markdownlint" },
-        elixir = { "credo" },
-        typescript = { "eslint", "biome" },
-        typescriptreact = { "eslint", "biome" },
-        python = { "pylint" },
-        ruby = { "rubocop" },
-        php = { "phpcs" },
-        codespell = { "codespell", "cspell" },
-        dockerfile = { "hadolint" },
-        json = { "jsonlint" },
-      }
+    local has_exe = function(exe)
+      return vim.fn.executable(exe) == 1
+    end
 
-      local function linter_exists(linter)
-        return vim.fn.executable(linter) == 1
-          or vim.fn.filereadable(vim.fn.getcwd() .. "/node_modules/.bin/" .. linter) == 1
-      end
+    local js_linters = {}
+    if has_exe 'eslint_d' then
+      table.insert(js_linters, 'eslint_d')
+    end
+    if has_exe 'eslint' then
+      table.insert(js_linters, 'eslint')
+    end
+    if has_exe 'biomejs' then
+      table.insert(js_linters, 'biomejs')
+    end
 
-      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-        group = lint_augroup,
-        callback = function()
-          local ft = vim.bo.filetype
-          local linters = lint.linters_by_ft[ft]
+    lint.linters_by_ft = {
+      markdown = { 'markdownlint' },
+      elixir = { 'credo' },
+      javascript = js_linters,
+      typescript = js_linters,
+      typescriptreact = js_linters,
+      javascriptreact = js_linters,
+      python = { 'pylint' },
+      php = { 'phpcs' },
+      dockerfile = { 'hadolint' },
+    }
 
-          if linters then
-            for _, linter in ipairs(linters) do
-              if linter_exists(linter) then
-                lint.try_lint()
-                break
-              end
-            end
-          end
-        end,
-      })
-
-      -- vim.keymap.set('n', '<leader>L', function()
-      --    local ft = vim.bo.filetype
-      --    local linters = lint.linters_by_ft[ft]
-
-      --    if linters then
-      --       for _, linter in ipairs(linters) do
-      --          if linter_exists(linter) then
-      --             lint.try_lint()
-      --             break
-      --          end
-      --       end
-      --    end
-      -- end, { desc = "Trigger linting for current file" })
-    end,
-  },
+    local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+      group = lint_augroup,
+      callback = function()
+        require('lint').try_lint()
+      end,
+    })
+  end,
 }
