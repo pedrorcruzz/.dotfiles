8370875676201baef61aa60f0bd0a2f54ad87dd7
pedrorcruzz <phrosa.dev@gmail.com>
2025-02-19 17:27:30 -0300

    feat: update plugin to new plugin

--- nvim/.config/nvim/lua/custom/plugins/editor/discord.lua
+++ nvim/.config/nvim/lua/custom/plugins/editor/discord.lua
@@ -1,25 +1,30 @@
 return {
-  'andweeb/presence.nvim',
-  opts = {
-    auto_update = true, -- Update activity based on autocmd events
-    neovim_image_text = 'The One True Text Editor', -- Text displayed when hovered over the Neovim image
-    main_image = 'neovim', -- Main image display (either "neovim" or "file")
-    client_id = '793271441293967371', -- Use your own Discord application client id (not recommended)
-    log_level = nil, -- Log messages at or above this level ("debug", "info", "warn", "error")
-    debounce_timeout = 10, -- Number of seconds to debounce events
-    enable_line_number = false, -- Displays the current line number instead of the current project
-    blacklist = {}, -- List of strings or Lua patterns that disable Rich Presence for matching files
-    buttons = true, -- Configure Rich Presence button(s)
-    file_assets = {}, -- Custom file asset definitions keyed by file names and extensions
-    show_time = true, -- Show the timer
+  'mistweaverco/discord.nvim',
+  event = 'VeryLazy',
+  config = function()
+    require('discord').setup {
+      -- General options
+      auto_connect = false, -- Automatically connect to Discord RPC
+      logo = 'https://logosrated.net/wp-content/uploads/parser/Neovim-Logo-1.png', -- "auto" or url
+      logo_tooltip = nil, -- nil or string
+      main_image = 'language', -- "language" or "logo"
+      client_id = '1233867420330889286', -- Use your own Discord application client id (not recommended)
+      log_level = nil, -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
+      debounce_timeout = 10, -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
+      blacklist = {}, -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
+      file_assets = {}, -- Custom file asset definitions keyed by file names and extensions
+      show_time = true, -- Show the timer
+      global_timer = true, -- if set false, timer will be reset on aucmds
 
-    -- Rich Presence text options
-    editing_text = 'Editing %s',
-    file_explorer_text = 'Browsing %s',
-    git_commit_text = 'Committing changes',
-    plugin_manager_text = 'Managing plugins',
-    reading_text = 'Reading %s',
-    workspace_text = 'Working on %s',
-    line_number_text = 'Line %s out of %s',
-  },
+      -- Rich Presence text options
+      editing_text = 'Editing %s', -- Format string rendered when an editable file is loaded in the buffer
+      file_explorer_text = 'Browsing %s', -- Format string rendered when browsing a file explorer
+      git_commit_text = 'Committing changes', -- Format string rendered when committing changes in git
+      plugin_manager_text = 'Managing plugins', -- Format string rendered when managing plugins
+      reading_text = 'Reading %s', -- Format string rendered when a read-only or unmodifiable file is loaded
+      workspace_text = 'Working on %s', -- Format string rendered when in a git repository
+      line_number_text = 'Line %s out of %s', -- Format string rendered when `enable_line_number` is set to true
+      terminal_text = 'Using Terminal', -- Format string rendered when in terminal mode
+    }
+  end,
 }
