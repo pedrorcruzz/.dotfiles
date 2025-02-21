1f66ce998694db47e82d284068df1756e16ec8ff
pedrorcruzz <phrosa.dev@gmail.com>
2025-02-20 23:04:17 -0300

    chore: change move alt

--- nvim/.config/nvim/lua/config/keybinds.lua
+++ nvim/.config/nvim/lua/config/keybinds.lua
@@ -1,7 +1,7 @@
 -- Mapeamentos de teclas
 vim.opt.hlsearch = true
-vim.keymap.set("i", "kj", "<Esc>", { noremap = true, silent = true })
-vim.keymap.set("i", "<Esc>", "<Esc>", { noremap = true, silent = true })
-vim.keymap.set("x", "<C-k>", ":move '<-2<CR>gv-gv", { noremap = true, silent = true })
-vim.keymap.set("x", "<C-j>", ":move '>+1<CR>gv-gv", { noremap = true, silent = true })
-vim.keymap.set("n", "<A-Down>", [[:m .+1<CR>==]], { noremap = true, silent = true })
+vim.keymap.set('i', 'kj', '<Esc>', { noremap = true, silent = true })
+vim.keymap.set('i', '<Esc>', '<Esc>', { noremap = true, silent = true })
+vim.keymap.set('x', '<C-o>', ":move '>+1<CR>gv-gv", { noremap = true, silent = true })
+vim.keymap.set('x', '<C-p>', ":move '<-2<CR>gv-gv", { noremap = true, silent = true })
+vim.keymap.set('n', '<A-Down>', [[:m .+1<CR>==]], { noremap = true, silent = true })
