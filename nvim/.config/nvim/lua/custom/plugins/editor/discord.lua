c82e303db2376d00fc0c28bd0ffe8a3012158e86
pedrorcruzz <phrosa.dev@gmail.com>
2025-02-21 13:10:32 -0300

    fix: if discord is closed then dont show notifications

--- nvim/.config/nvim/lua/custom/plugins/editor/discord.lua
+++ nvim/.config/nvim/lua/custom/plugins/editor/discord.lua
@@ -2,29 +2,33 @@ return {
   'mistweaverco/discord.nvim',
   event = 'VeryLazy',
   config = function()
-    require('discord').setup {
-      -- General options
-      auto_connect = false, -- Automatically connect to Discord RPC
-      logo = 'https://logosrated.net/wp-content/uploads/parser/Neovim-Logo-1.png', -- "auto" or url
-      logo_tooltip = nil, -- nil or string
-      main_image = 'language', -- "language" or "logo"
-      client_id = '1233867420330889286', -- Use your own Discord application client id (not recommended)
-      log_level = nil, -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
-      debounce_timeout = 10, -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
-      blacklist = {}, -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
-      file_assets = {}, -- Custom file asset definitions keyed by file names and extensions
-      show_time = true, -- Show the timer
-      global_timer = true, -- if set false, timer will be reset on aucmds
+    local is_discord_running = os.execute 'pgrep Discord > /dev/null'
 
-      -- Rich Presence text options
-      editing_text = 'Editing %s', -- Format string rendered when an editable file is loaded in the buffer
-      file_explorer_text = 'Browsing %s', -- Format string rendered when browsing a file explorer
-      git_commit_text = 'Committing changes', -- Format string rendered when committing changes in git
-      plugin_manager_text = 'Managing plugins', -- Format string rendered when managing plugins
-      reading_text = 'Reading %s', -- Format string rendered when a read-only or unmodifiable file is loaded
-      workspace_text = 'Working on %s', -- Format string rendered when in a git repository
-      line_number_text = 'Line %s out of %s', -- Format string rendered when `enable_line_number` is set to true
-      terminal_text = 'Using Terminal', -- Format string rendered when in terminal mode
+    if is_discord_running ~= 0 then
+      return
+    end
+
+    require('discord').setup {
+      auto_connect = false,
+      logo = 'https://logosrated.net/wp-content/uploads/parser/Neovim-Logo-1.png',
+      logo_tooltip = nil,
+      main_image = 'language',
+      client_id = '1233867420330889286',
+      log_level = nil,
+      debounce_timeout = 10,
+      blacklist = {},
+      file_assets = {},
+      show_time = true,
+      global_timer = true,
+
+      editing_text = 'Editing %s',
+      file_explorer_text = 'Browsing %s',
+      git_commit_text = 'Committing changes',
+      plugin_manager_text = 'Managing plugins',
+      reading_text = 'Reading %s',
+      workspace_text = 'Working on %s',
+      line_number_text = 'Line %s out of %s',
+      terminal_text = 'Using Terminal',
     }
   end,
 }
