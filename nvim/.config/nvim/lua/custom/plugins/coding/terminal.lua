032967d8e4ae0aafad259618aa6cde263dd198e8
pedrorcruzz <phrosa.dev@gmail.com>
2025-03-11 23:04:45 -0300

    auto-sync: 2025-03-11 23:04:45

--- nvim/.config/nvim/lua/custom/plugins/coding/terminal.lua
+++ nvim/.config/nvim/lua/custom/plugins/coding/terminal.lua
@@ -1,9 +1,9 @@
 return {
   {
-    "akinsho/toggleterm.nvim",
-    version = "*",
+    'akinsho/toggleterm.nvim',
+    version = '*',
     opts = {
-      size = 20,
+      size = 15,
       open_mapping = [[<c-\>]],
       hide_numbers = true,
       shade_terminals = true,
@@ -11,45 +11,45 @@ return {
       start_in_insert = true,
       insert_mappings = true,
       persist_size = false,
-      direction = "float",
+      direction = 'float',
       close_on_exit = true,
       shell = vim.o.shell,
       float_opts = {
-        border = "curved",
+        border = 'curved',
         winblend = 0,
         highlights = {
-          border = "Normal",
-          background = "Normal",
+          border = 'Normal',
+          background = 'Normal',
         },
       },
     },
     config = function(_, opts)
-      require("toggleterm").setup(opts)
+      require('toggleterm').setup(opts)
 
       -- Mapeamentos para diferentes direções
-      local Terminal = require("toggleterm.terminal").Terminal
+      local Terminal = require('toggleterm.terminal').Terminal
       local function create_terminal(direction, size, keymap)
-        local term = Terminal:new({ direction = direction, size = size })
-        vim.keymap.set({ "n", "t" }, keymap, function()
+        local term = Terminal:new { direction = direction, size = size }
+        vim.keymap.set({ 'n', 't' }, keymap, function()
           term:toggle()
-        end, { desc = "Toggle Terminal (" .. direction .. ")" })
+        end, { desc = 'Toggle Terminal (' .. direction .. ')' })
       end
 
-      create_terminal("horizontal", 20, "<S-x>")
+      create_terminal('horizontal', 20, '<S-x>')
       -- create_terminal("vertical", 60, "<C-]>")
-      create_terminal("float", nil, "<S-z>")
+      create_terminal('float', nil, '<S-z>')
 
       -- Atalho para LazyGit
-      local lazygit = Terminal:new({
-        cmd = "lazygit",
+      local lazygit = Terminal:new {
+        cmd = 'lazygit',
         hidden = true,
-        direction = "float",
-        float_opts = { border = "none", width = 100000, height = 100000 },
+        direction = 'float',
+        float_opts = { border = 'none', width = 100000, height = 100000 },
         on_open = function(_)
-          vim.cmd("startinsert!")
+          vim.cmd 'startinsert!'
         end,
         count = 99,
-      })
+      }
     end,
   },
 }
