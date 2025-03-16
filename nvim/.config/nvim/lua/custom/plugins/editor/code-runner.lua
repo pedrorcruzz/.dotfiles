a78f0be6f2738baa9e44256b5d6885304c3ed036
pedrorcruzz <phrosa.dev@gmail.com>
2025-03-16 11:19:56 -0300

    auto-sync: 2025-03-16 11:19:56

--- nvim/.config/nvim/lua/custom/plugins/editor/code-runner.lua
+++ nvim/.config/nvim/lua/custom/plugins/editor/code-runner.lua
@@ -12,6 +12,7 @@ return {
         typescript = 'bun "$fullFilePath"',
         go = 'go run "$fullFilePath"',
         elixir = 'elixir "$fullFilePath"',
+        php = 'php "$fullFilePath"',
       },
       buffer_height = 20, -- height in lines
       focus_back = false, -- whether to set the cursor back to the original window after running the code
