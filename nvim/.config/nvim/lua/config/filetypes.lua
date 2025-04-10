d90ac829d390661f3e6f6116b934e8ce714b3304
pedrorcruzz <phrosa.dev@gmail.com>
2025-04-10 10:20:27 -0300

    auto-sync: 2025-04-10 10:20:27

--- nvim/.config/nvim/lua/config/filetypes.lua
+++ nvim/.config/nvim/lua/config/filetypes.lua
@@ -29,3 +29,23 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.bo.expandtab = true
   end,
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'javascript' },
+  callback = function()
+    vim.bo.shiftwidth = 2
+    vim.bo.tabstop = 2
+    vim.bo.softtabstop = 2
+    vim.bo.expandtab = true
+  end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'typescript' },
+  callback = function()
+    vim.bo.shiftwidth = 2
+    vim.bo.tabstop = 2
+    vim.bo.softtabstop = 2
+    vim.bo.expandtab = true
+  end,
+})
