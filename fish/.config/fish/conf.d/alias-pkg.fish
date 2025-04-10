a69495ec7a36c492108301c2abf6462df300d30b
pedrorcruzz <phrosa.dev@gmail.com>
2025-04-10 09:22:24 -0300

    auto-sync: 2025-04-10 09:22:24

--- fish/.config/fish/conf.d/alias-pkg.fish
+++ fish/.config/fish/conf.d/alias-pkg.fish
@@ -1,14 +1,14 @@
 #bun
-abbr -a bun-remove-eslint "bun remove @eslint/js eslint eslint-plugin-react-hooks eslint-plugin-react-refresh"
+abbr -a bun-remove-eslint "bun remove @eslint/js eslint eslint-plugin-react-hooks eslint-plugin-react-refresh typescript-eslint"
 
 
 #npm
-abbr -a npm-remove-eslint "npm remove @eslint/js eslint eslint-plugin-react-hooks eslint-plugin-react-refresh"
+abbr -a npm-remove-eslint "npm remove @eslint/js eslint eslint-plugin-react-hooks eslint-plugin-react-refresh typescript-eslint"
 
 
 #pnpm
-abbr -a pnpm-remove-eslint "pnpm remove @eslint/js eslint eslint-plugin-react-hooks eslint-plugin-react-refresh"
+abbr -a pnpm-remove-eslint "pnpm remove @eslint/js eslint eslint-plugin-react-hooks eslint-plugin-react-refresh typescript-eslint"
 
 
 #yarn
-abbr -a yarn-remove-eslint "yarn remove @eslint/js eslint eslint-plugin-react-hooks eslint-plugin-react-refresh"
+abbr -a yarn-remove-eslint "yarn remove @eslint/js eslint eslint-plugin-react-hooks eslint-plugin-react-refresh typescript-eslint"
