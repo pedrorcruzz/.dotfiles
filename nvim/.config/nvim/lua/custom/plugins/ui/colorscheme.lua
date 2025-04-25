ec711b96fc4539218cc3d31e6b39459ddba9f289
pedrorcruzz <phrosa.dev@gmail.com>
2025-04-25 10:10:01 -0300

    auto-sync: 2025-04-25 10:10:01

--- nvim/.config/nvim/lua/custom/plugins/ui/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/ui/colorscheme.lua
@@ -8,7 +8,7 @@ return {
   {
     'datsfilipe/min-theme.nvim',
     config = function()
-      local transparent_background = true
+      local transparent_background = false
       require('min-theme').setup {
         transparent = transparent_background,
         italics = {
@@ -25,7 +25,7 @@ return {
           EndOfBuffer = { fg = '#1F1F1F', bg = 'none' },
           NormalFloat = { bg = transparent_background and 'none' or '#1F1F1F' },
           -- VertSplit = { fg = '#4c4c4c' },
-          VertSplit = { fg = transparent_background and '#1F1F1F' or '#1F1F1F' },
+          VertSplit = { fg = transparent_background and '#1F1F1F' or '#1A1A1A' },
 
           Visual = { bg = transparent_background and '#4c4c4c' or '#606060' },
           CursorLine = { bg = '#323232' },
