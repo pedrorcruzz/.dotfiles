c8d484667c893094d8763b6b85f00872a8cf5794
pedrorcruzz <phrosa.dev@gmail.com>
2025-04-28 00:10:43 -0300

    auto-sync: 2025-04-28 00:10:43

--- nvim/.config/nvim/lua/custom/plugins/env/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/plugins/env/lspconfig.lua
@@ -351,21 +351,21 @@ return { -- LSP Configuration & Plugins
       severity_sort = false,
     }
 
-    local signs = { Error = '󰅚 ', Warn = '󰀪 ', Hint = '󰌶 ', Info = ' ' }
-    for type, icon in pairs(signs) do
-      local hl = 'DiagnosticSign' .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-    end
-
-    vim.diagnostic.config {
-      virtual_text = {
-        spacing = 4,
-        prefix = '',
-      },
-      signs = true,
-      underline = true,
-      update_in_insert = false,
-      severity_sort = false,
-    }
+    -- local signs = { Error = '󰅚 ', Warn = '󰀪 ', Hint = '󰌶 ', Info = ' ' }
+    -- for type, icon in pairs(signs) do
+    --   local hl = 'DiagnosticSign' .. type
+    --   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    -- end
+    --
+    -- vim.diagnostic.config {
+    --   virtual_text = {
+    --     spacing = 4,
+    --     prefix = '',
+    --   },
+    --   signs = true,
+    --   underline = true,
+    --   update_in_insert = false,
+    --   severity_sort = false,
+    -- }
   end,
 }
