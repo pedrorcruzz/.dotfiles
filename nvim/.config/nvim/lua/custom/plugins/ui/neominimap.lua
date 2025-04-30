577be55b1bc2bad03e41263f6eb07991dc3de931
pedrorcruzz <phrosa.dev@gmail.com>
2025-04-30 16:10:01 -0300

    auto-sync: 2025-04-30 16:10:01

--- nvim/.config/nvim/lua/custom/plugins/ui/neominimap.lua
+++ nvim/.config/nvim/lua/custom/plugins/ui/neominimap.lua
@@ -7,27 +7,27 @@ return {
   keys = {
     -- Global Minimap Controls
     { '<leader>vm', '<cmd>Neominimap toggle<cr>', desc = 'Toggle global minimap' },
-    { '<leader>vo', '<cmd>Neominimap on<cr>', desc = 'Enable global minimap' },
-    { '<leader>vc', '<cmd>Neominimap off<cr>', desc = 'Disable global minimap' },
+    { '<leader>ve', '<cmd>Neominimap on<cr>', desc = 'Enable global minimap' },
+    { '<leader>vd', '<cmd>Neominimap off<cr>', desc = 'Disable global minimap' },
     { '<leader>vr', '<cmd>Neominimap refresh<cr>', desc = 'Refresh global minimap' },
 
     -- Window-Specific Minimap Controls
     { '<leader>vwt', '<cmd>Neominimap winToggle<cr>', desc = 'Toggle minimap for current window' },
     { '<leader>vwr', '<cmd>Neominimap winRefresh<cr>', desc = 'Refresh minimap for current window' },
-    { '<leader>vwo', '<cmd>Neominimap winOn<cr>', desc = 'Enable minimap for current window' },
-    { '<leader>vwc', '<cmd>Neominimap winOff<cr>', desc = 'Disable minimap for current window' },
+    { '<leader>vwe', '<cmd>Neominimap winOn<cr>', desc = 'Enable minimap for current window' },
+    { '<leader>vwd', '<cmd>Neominimap winOff<cr>', desc = 'Disable minimap for current window' },
 
     -- Tab-Specific Minimap Controls
     { '<leader>vtt', '<cmd>Neominimap tabToggle<cr>', desc = 'Toggle minimap for current tab' },
     { '<leader>vtr', '<cmd>Neominimap tabRefresh<cr>', desc = 'Refresh minimap for current tab' },
-    { '<leader>vto', '<cmd>Neominimap tabOn<cr>', desc = 'Enable minimap for current tab' },
-    { '<leader>vtc', '<cmd>Neominimap tabOff<cr>', desc = 'Disable minimap for current tab' },
+    { '<leader>vte', '<cmd>Neominimap tabOn<cr>', desc = 'Enable minimap for current tab' },
+    { '<leader>vtd', '<cmd>Neominimap tabOff<cr>', desc = 'Disable minimap for current tab' },
 
     -- Buffer-Specific Minimap Controls
     { '<leader>vbt', '<cmd>Neominimap bufToggle<cr>', desc = 'Toggle minimap for current buffer' },
     { '<leader>vbr', '<cmd>Neominimap bufRefresh<cr>', desc = 'Refresh minimap for current buffer' },
-    { '<leader>vbo', '<cmd>Neominimap bufOn<cr>', desc = 'Enable minimap for current buffer' },
-    { '<leader>vbc', '<cmd>Neominimap bufOff<cr>', desc = 'Disable minimap for current buffer' },
+    { '<leader>vbe', '<cmd>Neominimap bufOn<cr>', desc = 'Enable minimap for current buffer' },
+    { '<leader>vbd', '<cmd>Neominimap bufOff<cr>', desc = 'Disable minimap for current buffer' },
 
     ---Focus Controls
     { '<leader>vf', '<cmd>Neominimap focus<cr>', desc = 'Focus on minimap' },
