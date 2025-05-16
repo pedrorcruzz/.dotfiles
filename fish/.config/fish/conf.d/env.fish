f3595dc836bcabad9e60cc8db1f861f22169b2b8
pedrorcruzz <phrosa.dev@gmail.com>
2025-05-16 10:05:14 -0300

    auto-sync: 2025-05-16 10:05:14

--- fish/.config/fish/conf.d/env.fish
+++ fish/.config/fish/conf.d/env.fish
@@ -7,6 +7,8 @@ fish_add_path "/Users/phrosa/Library/Application Support/Herd/bin/"
 set -gx HERD_PHP_84_INI_SCAN_DIR "/Users/phrosa/Library/Application Support/Herd/config/php/84/"
 
 
+# Npm
+
 # Bun
 set -gx BUN_INSTALL "$HOME/.bun"
 fish_add_path "$BUN_INSTALL/bin"
@@ -20,6 +22,8 @@ set -gx YARN_HOME "$HOME/.yarn"
 fish_add_path "$YARN_HOME/bin"
 
 set -gx PATH $HOME/.asdf/shims $PATH
+fish_add_path $HOME/.asdf/installs/nodejs/$(asdf current nodejs | awk '{print $2}')/bin
+fish_add_path $HOME/.asdf/installs/nodejs/$(asdf current nodejs | awk '{print $2}')/lib/node_modules/npm/bin
 set -gx PATH $HOME/.asdf/bin $PATH	
 set -gx PATH "$HOME/.asdf/installs/python/3.13.2/bin" $PATH
 
@@ -29,3 +33,5 @@ set -x LDFLAGS "-L/opt/homebrew/opt/tcl-tk/lib"
 set -x CPPFLAGS "-I/opt/homebrew/opt/tcl-tk/include"
 set -x PKG_CONFIG_PATH "/opt/homebrew/opt/tcl-tk/lib/pkgconfig"
 set -x PATH /opt/homebrew/opt/tcl-tk/bin $PATH
+
+
