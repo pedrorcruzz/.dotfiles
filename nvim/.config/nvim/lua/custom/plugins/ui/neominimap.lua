e18a418f295151f92710fc5138f282abc5cab962
pedrorcruzz <phrosa.dev@gmail.com>
2025-06-29 18:17:29 -0300

    auto-sync: 2025-06-29 18:17:29

--- nvim/.config/nvim/lua/custom/plugins/ui/neominimap.lua
+++ nvim/.config/nvim/lua/custom/plugins/ui/neominimap.lua
@@ -6,33 +6,33 @@ return {
   cmd = { 'Neominimap' },
   keys = {
     -- Global Minimap Controls
-    { '<leader>,m', '<cmd>Neominimap Toggle<cr>', desc = 'Toggle global minimap' },
-    { '<leader>,e', '<cmd>Neominimap Enable<cr>', desc = 'Enable global minimap' },
-    { '<leader>,d', '<cmd>Neominimap Disable<cr>', desc = 'Disable global minimap' },
-    { '<leader>,r', '<cmd>Neominimap Refresh<cr>', desc = 'Refresh global minimap' },
+    { '<leader>zm', '<cmd>Neominimap Toggle<cr>', desc = 'Toggle global minimap' },
+    { '<leader>ze', '<cmd>Neominimap Enable<cr>', desc = 'Enable global minimap' },
+    { '<leader>zd', '<cmd>Neominimap Disable<cr>', desc = 'Disable global minimap' },
+    { '<leader>zr', '<cmd>Neominimap Refresh<cr>', desc = 'Refresh global minimap' },
 
     -- Window-Specific Minimap Controls
-    { '<leader>,wt', '<cmd>Neominimap winToggle<cr>', desc = 'Toggle minimap for current window' },
-    { '<leader>,wr', '<cmd>Neominimap winRefresh<cr>', desc = 'Refresh minimap for current window' },
-    { '<leader>,we', '<cmd>Neominimap winEnable<cr>', desc = 'Enable minimap for current window' },
-    { '<leader>,wd', '<cmd>Neominimap winDisable<cr>', desc = 'Disable minimap for current window' },
+    { '<leader>zwt', '<cmd>Neominimap winToggle<cr>', desc = 'Toggle minimap for current window' },
+    { '<leader>zwr', '<cmd>Neominimap winRefresh<cr>', desc = 'Refresh minimap for current window' },
+    { '<leader>zwe', '<cmd>Neominimap winEnable<cr>', desc = 'Enable minimap for current window' },
+    { '<leader>zwd', '<cmd>Neominimap winDisable<cr>', desc = 'Disable minimap for current window' },
 
     -- Tab-Specific Minimap Controls
-    { '<leader>,tt', '<cmd>Neominimap tabToggle<cr>', desc = 'Toggle minimap for current tab' },
-    { '<leader>,tr', '<cmd>Neominimap tabRefresh<cr>', desc = 'Refresh minimap for current tab' },
-    { '<leader>,te', '<cmd>Neominimap tabOn<cr>', desc = 'Enable minimap for current tab' },
-    { '<leader>,td', '<cmd>Neominimap tabOff<cr>', desc = 'Disable minimap for current tab' },
+    { '<leader>ztt', '<cmd>Neominimap tabToggle<cr>', desc = 'Toggle minimap for current tab' },
+    { '<leader>ztr', '<cmd>Neominimap tabRefresh<cr>', desc = 'Refresh minimap for current tab' },
+    { '<leader>zte', '<cmd>Neominimap tabOn<cr>', desc = 'Enable minimap for current tab' },
+    { '<leader>ztd', '<cmd>Neominimap tabOff<cr>', desc = 'Disable minimap for current tab' },
 
     -- Buffer-Specific Minimap Controls
-    { '<leader>,bt', '<cmd>Neominimap BufToggle<cr>', desc = 'Toggle minimap for current buffer' },
-    { '<leader>,br', '<cmd>Neominimap bufRefresh<cr>', desc = 'Refresh minimap for current buffer' },
-    { '<leader>,be', '<cmd>Neominimap BufEnable<cr>', desc = 'Enable minimap for current buffer' },
-    { '<leader>,bd', '<cmd>Neominimap BufDisable<cr>', desc = 'Disable minimap for current buffer' },
+    { '<leader>zbt', '<cmd>Neominimap BufToggle<cr>', desc = 'Toggle minimap for current buffer' },
+    { '<leader>zbr', '<cmd>Neominimap bufRefresh<cr>', desc = 'Refresh minimap for current buffer' },
+    { '<leader>zbe', '<cmd>Neominimap BufEnable<cr>', desc = 'Enable minimap for current buffer' },
+    { '<leader>zbd', '<cmd>Neominimap BufDisable<cr>', desc = 'Disable minimap for current buffer' },
 
     -- Focus Controls
-    { '<leader>,f', '<cmd>Neominimap Focus<cr>', desc = 'Focus on minimap' },
-    { '<leader>,u', '<cmd>Neominimap Unfocus<cr>', desc = 'Unfocus minimap' },
-    { '<leader>,s', '<cmd>Neominimap ToggleFocus<cr>', desc = 'Switch focus on minimap' },
+    { '<leader>zf', '<cmd>Neominimap Focus<cr>', desc = 'Focus on minimap' },
+    { '<leader>zu', '<cmd>Neominimap Unfocus<cr>', desc = 'Unfocus minimap' },
+    { '<leader>zs', '<cmd>Neominimap ToggleFocus<cr>', desc = 'Switch focus on minimap' },
   },
   init = function()
     vim.opt.wrap = false
