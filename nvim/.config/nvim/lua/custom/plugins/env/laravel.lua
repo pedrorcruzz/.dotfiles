5f3ebfde72335a65a5cc01d09261a892ad30fadc
pedrorcruzz <phrosa.dev@gmail.com>
2025-06-29 23:47:22 -0300

    auto-sync: 2025-06-29 23:47:22

--- nvim/.config/nvim/lua/custom/plugins/env/laravel.lua
+++ nvim/.config/nvim/lua/custom/plugins/env/laravel.lua
@@ -8,32 +8,32 @@ return {
   ft = { 'php', 'blade' },
   keys = {
     -- Core
-    { '<leader>pla', ':Artisan<CR>', desc = 'Laravel Artisan' },
-    { '<leader>pls', ':LaravelStatus<CR>', desc = 'Laravel Status' },
-    { '<leader>plr', ':LaravelRoute<CR>', desc = 'Laravel Routes' },
-    { '<leader>plg', ':LaravelMake<CR>', desc = 'Laravel Make' },
+    { '<leader>pla', '<cmd>Artisan<cr>', desc = 'Laravel Artisan' },
+    { '<leader>pls', '<cmd>LaravelStatus<cr>', desc = 'Laravel Status' },
+    { '<leader>plr', '<cmd>LaravelRoute<cr>', desc = 'Laravel Routes' },
+    { '<leader>plg', '<cmd>LaravelMake<cr>', desc = 'Laravel Make' },
 
     -- Composer
-    { '<leader>plci', ':Composer install<CR>', desc = 'Composer Install' },
-    { '<leader>plcu', ':Composer update<CR>', desc = 'Composer Update' },
-    { '<leader>plcr', ':ComposerRequire ', desc = 'Composer Require Package' },
-    { '<leader>plcd', ':ComposerDependencies<CR>', desc = 'Composer Dependencies' },
-    { '<leader>plcx', ':ComposerRemove ', desc = 'Composer Remove Package' },
+    { '<leader>plci', '<cmd>Composer install<cr>', desc = 'Composer Install' },
+    { '<leader>plcu', '<cmd>Composer update<cr>', desc = 'Composer Update' },
+    { '<leader>plcr', '<cmd>ComposerRequire<cr> ', desc = 'Composer Require Package' },
+    { '<leader>plcd', '<cmd>ComposerDependencies<cr>', desc = 'Composer Dependencies' },
+    { '<leader>plcx', '<cmd>ComposerRemove<cr> ', desc = 'Composer Remove Package' },
 
     -- Navigation
-    { '<leader>ple', ':LaravelController ', desc = 'Goto Controller' },
-    { '<leader>plm', ':LaravelModel ', desc = 'Goto Model' },
-    { '<leader>plv', ':LaravelView ', desc = 'Goto View' },
-    -- { '<leader>plg', ':LaravelGoto<CR>', desc = 'Laravel Goto Definition' },
+    { '<leader>ple', '<cmd>LaravelController<cr> ', desc = 'Goto Controller' },
+    { '<leader>plm', '<cmd>LaravelModel<cr> ', desc = 'Goto Model' },
+    { '<leader>plv', '<cmd>LaravelView<cr> ', desc = 'Goto View' },
+    -- { '<leader>plg', '<cmd>LaravelGoto<cr>', desc = 'Laravel Goto Definition' },
 
     -- Diagrams
-    { '<leader>plds', ':LaravelSchema<CR>', desc = 'Show DB Schema' },
-    { '<leader>plde', ':LaravelSchemaExport<CR>', desc = 'Export DB Schema' },
-    { '<leader>plda', ':LaravelArchitecture<CR>', desc = 'Show App Architecture' },
+    { '<leader>plds', '<cmd>LaravelSchema<cr>', desc = 'Show DB Schema' },
+    { '<leader>plde', '<cmd>LaravelSchemaExport<cr>', desc = 'Export DB Schema' },
+    { '<leader>plda', '<cmd>LaravelArchitecture<cr>', desc = 'Show App Architecture' },
 
     -- Cache
-    { '<leader>plxc', ':LaravelClearCache<CR>', desc = 'Clear Laravel Cache' },
-    { '<leader>plxo', ':LaravelCompletions ', desc = 'Show Laravel Completions' },
+    { '<leader>plxc', '<cmd>LaravelClearCache<cr>', desc = 'Clear Laravel Cache' },
+    { '<leader>plxo', '<cmd>LaravelCompletions<cr> ', desc = 'Show Laravel Completions' },
   },
   config = function()
     require('laravel').setup {
