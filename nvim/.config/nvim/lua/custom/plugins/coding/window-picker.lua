f4c911d80feacca6b41659821fa3f5c10cc01e32
pedrorcruzz <phrosa.dev@gmail.com>
2025-07-19 20:52:17 -0300

    auto-sync: 2025-07-19 20:52:17

--- nvim/.config/nvim/lua/custom/plugins/coding/window-picker.lua
+++ nvim/.config/nvim/lua/custom/plugins/coding/window-picker.lua
@@ -1,7 +1,6 @@
 return {
   'yorickpeterse/nvim-window',
   keys = {
-    { '<leader>cn', "<cmd>lua require('nvim-window').pick()<cr>", desc = 'Pick a Window' },
     { '<leader>m', "<cmd>lua require('nvim-window').pick()<cr>", desc = 'Pick a Window' },
   },
   config = true,
