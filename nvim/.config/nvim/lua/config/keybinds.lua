cc160987ab48e89e1ce35e5364678e19c6a87dcb
pedrorcruzz <phrosa.dev@gmail.com>
2025-08-20 15:03:05 -0300

    auto-sync: 2025-08-20 15:03:05

--- nvim/.config/nvim/lua/config/keybinds.lua
+++ nvim/.config/nvim/lua/config/keybinds.lua
@@ -18,3 +18,5 @@ vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<BS>', '<c-6>', { desc = 'Switch to last buffer' })
