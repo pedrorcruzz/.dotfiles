4cfc0007ca4705c0f303bde82f876f93ad1a9349
pedrorcruzz <phrosa.dev@gmail.com>
2025-09-03 15:15:26 -0300

    auto-sync: 2025-09-03 15:15:25

--- nvim/.config/nvim/lua/custom/plugins/ui/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/ui/colorscheme.lua
@@ -1,3 +1,13 @@
+-- Custom Colorscheme
+vim.api.nvim_create_autocmd('ColorScheme', {
+  pattern = '*',
+  callback = function()
+    vim.api.nvim_set_hl(0, 'BlinkCmpMenuBorder', { fg = '#4c4c4c' })
+    vim.api.nvim_set_hl(0, 'BlinkCmpDocBorder', { fg = '#4c4c4c' })
+    vim.api.nvim_set_hl(0, 'BlinkCmpSignatureHelpBorder', { fg = '#4c4c4c' })
+  end,
+})
+
 return {
   {
     'LazyVim/LazyVim',
