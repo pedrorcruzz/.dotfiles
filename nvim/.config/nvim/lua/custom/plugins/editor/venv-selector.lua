6b5ea7d229c61c13aa424dec296bed0833c77786
pedrorcruzz <phrosa.dev@gmail.com>
2025-10-04 21:54:14 -0300

    auto-sync: 2025-10-04 21:54:14

--- nvim/.config/nvim/lua/custom/plugins/editor/venv-selector.lua
+++ nvim/.config/nvim/lua/custom/plugins/editor/venv-selector.lua
@@ -1,24 +1,14 @@
 return {
   'linux-cultist/venv-selector.nvim',
-  branch = 'regexp',
-  lazy = true,
-  cmd = { 'VenvSelect', 'VenvSelectCached' },
-  ft = { 'python' },
-  keys = {
-    { '<leader>lxs', '<cmd>VenvSelect<cr>', desc = 'Select virtual environment' },
-  },
   dependencies = {
     'neovim/nvim-lspconfig',
-    'mfussenegger/nvim-dap',
-    'mfussenegger/nvim-dap-python',
-    -- telescope (opcional se quiser fuzzy finder para ambientes virtuais)
-    -- {
-    --   'nvim-telescope/telescope.nvim',
-    --   branch = '0.1.x',
-    --   dependencies = { 'nvim-lua/plenary.nvim' },
-    -- },
   },
-  config = function()
-    require('venv-selector').setup()
-  end,
+  ft = 'python', -- Load when opening Python files
+  keys = {
+    { '<leader>lxs', '<cmd>VenvSelect<cr>' }, -- Open picker on keymap
+  },
+  opts = { -- this can be an empty lua table - just showing below for clarity.
+    search = {}, -- if you add your own searches, they go here.
+    options = {}, -- if you add plugin options, they go here.
+  },
 }
