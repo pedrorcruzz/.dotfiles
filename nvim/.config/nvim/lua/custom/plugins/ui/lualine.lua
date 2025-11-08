e9aeb2ca32671f8d2df5f31898b42db69dd8732d
pedrorcruzz <phrosa.dev@gmail.com>
2025-11-07 21:52:05 -0300

    auto-sync: 2025-11-07 21:52:05

--- nvim/.config/nvim/lua/custom/plugins/ui/lualine.lua
+++ nvim/.config/nvim/lua/custom/plugins/ui/lualine.lua
@@ -1,68 +1,176 @@
 local lualine_visible = true
 local use_custom_theme = true
 
-local function load_colors()
-  local use_custom_only_bg_min = true
-  local use_custom_multiple_bg_min = false
-
-  if use_custom_only_bg_min then
-    return require 'custom.plugins.ui.lualine_themes.custom_only_bg_min'
-  elseif use_custom_multiple_bg_min then
-    return require 'custom.plugins.ui.lualine_themes.custom_multiple_bg_min'
-  else
-    return require 'custom.plugins.ui.lualine_themes.default'
-  end
-end
-
 local function create_theme()
-  if not use_custom_theme then
-    return 'auto'
-  end
+  local custom_only_bg_min = true
+  local custom_multiple_bg_min = false
+  local colors = {}
 
-  local colors = load_colors()
+  if custom_only_bg_min then
+    colors = {
+      min_fg_branch = '#a9a9a9',
+      min_bg_branch = '#2d2d2d',
+      min_bar_bg = nil,
+      min_git = '#FFFFFF',
+      min_y_bg = '#2d2d2d',
+      min_y_fg = '#a9a9a9',
+      min_uni_fg_color = '#FEFEFE',
+      min_uni_bg_color = '#1a1a1a',
+      min_uni_bg_color_z = nil,
+    }
+  elseif custom_multiple_bg_min then
+    colors = {
+      multiple_min_fg_branch = '#a9a9a9',
+      multiple_min_bg_branch = '#2d2d2d',
+      vesper_bar_bg = nil,
+      vesper_git = '#ECEFF4',
+      vesper_y_bg = '#101010',
+      vesper_y_fg = '#b0c4de',
+      vesper_uni_fg_color = '#D8DEE9',
+      vesper_uni_bg_color = '#101010',
+      vesper_uni_bg_color_z = '#3B4252',
+    }
+  else
+    colors = {
+      default_branch_fg = '#abb2bf',
+      default_branch_bg = nil,
+      default_fg_color = '#1A1A1A',
+      default_y_bg = nil,
+      default_bar_bg = nil,
+      default_y_fg = '#a9a9a9',
+      default_normal = '#64BAFF',
+      default_insert = '#FF7081',
+      default_visual = '#B990F7',
+      default_replace = '#ffa066',
+      default_command = '#75bf63',
+      default_location_fg = '#FFFFFF',
+      default_location_bg = nil,
+    }
+  end
 
   return {
     normal = {
-      a = { fg = colors.a_fg, bg = colors.a_bg, gui = 'bold' },
-      b = { fg = colors.b_fg, bg = colors.b_bg, gui = 'bold' },
-      c = { fg = colors.c_fg, bg = colors.c_bg },
-      y = { fg = colors.y_fg, bg = colors.y_bg, gui = 'bold' },
-      z = { fg = colors.z_fg, bg = colors.z_bg, gui = 'bold' },
+      a = {
+        fg = colors.min_uni_fg_color or colors.vesper_uni_fg_color or colors.default_fg_color,
+        bg = colors.min_uni_bg_color or colors.vesper_uni_bg_color or colors.default_normal,
+        gui = 'bold',
+      },
+      b = {
+        fg = colors.min_fg_branch or colors.multiple_min_fg_branch or colors.default_branch_fg,
+        bg = colors.min_bg_branch or colors.multiple_min_bg_branch or colors.default_branch_bg,
+        gui = 'bold',
+      },
+      c = { fg = colors.min_git or colors.vesper_git, bg = colors.min_bar_bg or colors.vesper_bar_bg or colors.default_bar_bg },
+      y = {
+        fg = colors.min_y_fg or colors.vesper_y_fg or colors.default_y_fg,
+        bg = colors.min_y_bg or colors.vesper_y_bg or colors.default_y_bg,
+        gui = 'bold',
+      },
+      z = {
+        fg = colors.min_uni_fg_color or colors.vesper_uni_fg_color or colors.default_fg_color,
+        bg = colors.min_uni_bg_color or colors.vesper_uni_bg_color or colors.default_normal,
+        gui = 'bold',
+      },
     },
     insert = {
-      a = { fg = colors.a_fg, bg = colors.insert_bg, gui = 'bold' },
-      b = { fg = colors.b_fg, bg = colors.b_bg, gui = 'bold' },
-      c = { fg = colors.c_fg, bg = colors.c_bg },
-      y = { fg = colors.y_fg, bg = colors.y_bg, gui = 'bold' },
-      z = { fg = colors.z_fg, bg = colors.insert_bg, gui = 'bold' },
+      a = {
+        fg = colors.min_uni_fg_color or colors.vesper_uni_fg_color or colors.default_fg_color,
+        bg = colors.min_uni_bg_color or colors.vesper_uni_bg_color or colors.default_insert,
+        gui = 'bold',
+      },
+      b = {
+        fg = colors.min_fg_branch or colors.multiple_min_fg_branch or colors.default_branch_fg,
+        bg = colors.min_bg_branch or colors.multiple_min_bg_branch or colors.default_branch_bg,
+        gui = 'bold',
+      },
+      c = { fg = colors.min_git or colors.vesper_git, bg = colors.min_bar_bg or colors.vesper_bar_bg or colors.default_bar_bg },
+      y = {
+        fg = colors.min_y_fg or colors.vesper_y_fg or colors.default_y_fg,
+        bg = colors.min_y_bg or colors.vesper_y_bg or colors.default_y_bg,
+        gui = 'bold',
+      },
+      z = {
+        fg = colors.min_uni_fg_color or colors.vesper_uni_fg_color or colors.default_fg_color,
+        bg = colors.min_uni_bg_color or colors.vesper_uni_bg_color or colors.default_insert,
+        gui = 'bold',
+      },
     },
     visual = {
-      a = { fg = colors.a_fg, bg = colors.visual_bg, gui = 'bold' },
-      b = { fg = colors.b_fg, bg = colors.b_bg, gui = 'bold' },
-      c = { fg = colors.c_fg, bg = colors.c_bg },
-      y = { fg = colors.y_fg, bg = colors.y_bg, gui = 'bold' },
-      z = { fg = colors.z_fg, bg = colors.visual_bg, gui = 'bold' },
+      a = {
+        fg = colors.min_uni_fg_color or colors.vesper_uni_fg_color or colors.default_fg_color,
+        bg = colors.min_uni_bg_color or colors.vesper_uni_bg_color or colors.default_visual,
+        gui = 'bold',
+      },
+      b = {
+        fg = colors.min_fg_branch or colors.multiple_min_fg_branch or colors.default_branch_fg,
+        bg = colors.min_bg_branch or colors.multiple_min_bg_branch or colors.default_branch_bg,
+        gui = 'bold',
+      },
+      c = { fg = colors.min_git or colors.vesper_git, bg = colors.min_bar_bg or colors.vesper_bar_bg or colors.default_bar_bg },
+      y = {
+        fg = colors.min_y_fg or colors.vesper_y_fg or colors.default_y_fg,
+        bg = colors.min_y_bg or colors.vesper_y_bg or colors.default_y_bg,
+        gui = 'bold',
+      },
+      z = {
+        fg = colors.min_uni_fg_color or colors.vesper_uni_fg_color or colors.default_fg_color,
+        bg = colors.min_uni_bg_color or colors.vesper_uni_bg_color or colors.default_visual,
+        gui = 'bold',
+      },
     },
     replace = {
-      a = { fg = colors.a_fg, bg = colors.replace_bg, gui = 'bold' },
-      b = { fg = colors.b_fg, bg = colors.b_bg, gui = 'bold' },
-      c = { fg = colors.c_fg, bg = colors.c_bg },
-      y = { fg = colors.y_fg, bg = colors.y_bg, gui = 'bold' },
-      z = { fg = colors.z_fg, bg = colors.replace_bg, gui = 'bold' },
+      a = {
+        fg = colors.min_uni_fg_color or colors.vesper_uni_fg_color or colors.default_fg_color,
+        bg = colors.min_uni_bg_color or colors.vesper_uni_bg_color or colors.default_replace,
+        gui = 'bold',
+      },
+      b = {
+        fg = colors.min_fg_branch or colors.multiple_min_fg_branch or colors.default_branch_fg,
+        bg = colors.min_bg_branch or colors.multiple_min_bg_branch or colors.default_branch_bg,
+        gui = 'bold',
+      },
+      c = { fg = colors.min_git or colors.vesper_git, bg = colors.min_bar_bg or colors.vesper_bar_bg or colors.default_bar_bg },
+      y = {
+        fg = colors.min_y_fg or colors.vesper_y_fg or colors.default_y_fg,
+        bg = colors.min_y_bg or colors.vesper_y_bg or colors.default_y_bg,
+        gui = 'bold',
+      },
+      z = {
+        fg = colors.min_uni_fg_color or colors.vesper_uni_fg_color or colors.default_fg_color,
+        bg = colors.min_uni_bg_color or colors.vesper_uni_bg_color or colors.default_replace,
+        gui = 'bold',
+      },
     },
     command = {
-      a = { fg = colors.a_fg, bg = colors.command_bg, gui = 'bold' },
-      b = { fg = colors.b_fg, bg = colors.b_bg, gui = 'bold' },
-      c = { fg = colors.c_fg, bg = colors.c_bg },
-      y = { fg = colors.y_fg, bg = colors.y_bg, gui = 'bold' },
-      z = { fg = colors.z_fg, bg = colors.command_bg, gui = 'bold' },
+      a = {
+        fg = colors.min_uni_fg_color or colors.vesper_uni_fg_color or colors.default_fg_color,
+        bg = colors.min_uni_bg_color or colors.vesper_uni_bg_color or colors.default_command,
+        gui = 'bold',
+      },
+      b = {
+        fg = colors.min_fg_branch or colors.multiple_min_fg_branch or colors.default_branch_fg,
+        bg = colors.min_bg_branch or colors.multiple_min_bg_branch or colors.default_branch_bg,
+        gui = 'bold',
+      },
+      c = { fg = colors.min_git or colors.vesper_git, bg = colors.min_bar_bg or colors.vesper_bar_bg or colors.default_bar_bg },
+      y = {
+        fg = colors.min_y_fg or colors.vesper_y_fg or colors.default_y_fg,
+        bg = colors.min_y_bg or colors.vesper_y_bg or colors.default_y_bg,
+        gui = 'bold',
+      },
+      z = {
+        fg = colors.min_uni_fg_color or colors.vesper_uni_fg_color or colors.default_fg_color,
+        bg = colors.min_uni_bg_color or colors.vesper_uni_bg_color or colors.default_command,
+        gui = 'bold',
+      },
     },
   }
 end
 
+-- aplica tema apenas se use_custom_theme == true
 local lualine_config = {
   options = {
-    theme = create_theme(),
+    theme = use_custom_theme and create_theme() or 'auto',
     section_separators = { left = '', right = '' },
     component_separators = { left = '', right = '' },
   },
@@ -71,7 +179,7 @@ local lualine_config = {
       {
         'mode',
         fmt = function(mode)
-          return '󰅶 ' .. mode --󰅶 󱄛
+          return '󰅶 ' .. mode
         end,
         separator = { left = '', right = '' },
         -- separator = { left = '', right = '' },
@@ -87,15 +195,12 @@ local lualine_config = {
         symbols = { added = ' ', modified = ' ', removed = ' ', untracked = '󰝤' },
       },
     },
-    lualine_x = {
-      'diagnostics',
-    },
+    lualine_x = { 'diagnostics' },
     lualine_y = {
       'copilot',
-
       {
         'lsp_status',
-        icon = '',
+        icon = '',
         symbols = {
           spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' },
           done = '✓',
@@ -103,7 +208,6 @@ local lualine_config = {
         },
         ignore_lsp = { 'copilot', 'tailwindcss' },
       },
-
       'progress',
       { 'filename', symbols = { modified = '●', readonly = '' } },
       'filetype',
