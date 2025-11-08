d23e18cbb36ee99dd00dcf13e0b1133a795e8cba
pedrorcruzz <phrosa.dev@gmail.com>
2025-11-08 15:19:01 -0300

    auto-sync: 2025-11-08 15:19:01

--- nvim/.config/nvim/lua/custom/plugins/ui/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/ui/colorscheme.lua
@@ -21,7 +21,7 @@ return {
       end)
     end,
     opts = {
-      colorscheme = 'rose-pine', -- min-theme,rusticated,rose-pine,kanagawa-dragon, peachpuff, vesper, catppuccin-mocha, gruvbox, emperor, marduk, gorgoroth, khold, venom
+      colorscheme = 'rose-pine', -- min-theme,rusticated,rose-pine,kanagawa-dragon, peachpuff, vesper, catppuccin-mocha, gruvbox
     },
   },
 }
