1968ff44e654a99e00740ae1b4b812b1648957f8
pedrorcruzz <phrosa.dev@gmail.com>
2025-11-08 13:15:06 -0300

    auto-sync: 2025-11-08 13:15:06

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -234,24 +234,6 @@ if enable_custom_reference_highlights then
   })
 end
 
--- Restore last cursor position
-local enable_restore_last_cursor = false
-
-if enable_restore_last_cursor then
-  local restore_cursor_group = vim.api.nvim_create_augroup('RestoreLastCursor', { clear = true })
-
-  vim.api.nvim_create_autocmd('BufReadPost', {
-    group = restore_cursor_group,
-    callback = function()
-      local mark = vim.api.nvim_buf_get_mark(0, '"')
-      local lcount = vim.api.nvim_buf_line_count(0)
-      if mark[1] > 0 and mark[1] <= lcount then
-        pcall(vim.api.nvim_win_set_cursor, 0, mark)
-      end
-    end,
-  })
-end
-
 -- DBUI NO FOLDING
 local enable_dbout_no_folding = true
 
