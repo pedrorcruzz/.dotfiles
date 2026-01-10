6782b0ca39dd6c6fc70746d99f2c8fa5642db5ab
pedrorcruzz <phrosa.dev@gmail.com>
2026-01-10 01:15:15 -0300

    auto-sync: 2026-01-10 01:15:15

--- nvim/.config/nvim/lua/custom/plugins/ui/incline.lua
+++ nvim/.config/nvim/lua/custom/plugins/ui/incline.lua
@@ -1,17 +1,17 @@
-local incline_active = false
+local incline_active = true
 
 return {
   'b0o/incline.nvim',
   dependencies = {
     { 'SmiteshP/nvim-navic', setup = { highlight = true } },
-    'nvim-tree/nvim-web-devicons',
+    'echasnovski/mini.icons',
     'cbochs/grapple.nvim',
   },
   event = 'VeryLazy',
   config = function()
     local helpers = require 'incline.helpers'
     local navic = require 'nvim-navic'
-    local devicons = require 'nvim-web-devicons'
+    local icons = require 'mini.icons'
     local grapple = require 'grapple'
     -- local harpoon = require 'harpoon'
 
@@ -28,10 +28,11 @@ return {
           filename = '[No Name]'
         end
 
-        local ft_icon, ft_color = devicons.get_icon_color(filename)
+        local ft_icon, ft_hl = icons.get('file', filename)
+
         local modified = vim.bo[props.buf].modified and ' ●' or ''
         local res = {
-          ft_icon and { ' ', ft_icon, ' ', guibg = nil, guifg = ft_color } or '',
+          ft_icon and { ' ', ft_icon, ' ', guibg = nil, group = ft_hl } or '',
           ' ',
           { filename .. modified, gui = 'bold' },
           ' ',
@@ -66,40 +67,39 @@ return {
       end,
 
       -- local function get_harpoon_items()
-      --       local marks = harpoon:list().items
-      --       local current_file_path = vim.fn.expand '%:p:. '
-      --       local label = {}
+      --        local marks = harpoon:list().items
+      --        local current_file_path = vim.fn.expand '%:p:. '
+      --        local label = {}
       --
-      --       for id, item in ipairs(marks) do
-      --         if item.value == current_file_path then
-      --           table.insert(label, { id .. ' ', guifg = '#FFFFFF', gui = 'bold' })
-      --         else
-      --           table.insert(label, { id .. ' ', guifg = '#434852' })
-      --         end
-      --       end
+      --        for id, item in ipairs(marks) do
+      --          if item.value == current_file_path then
+      --            table.insert(label, { id .. ' ', guifg = '#FFFFFF', gui = 'bold' })
+      --          else
+      --            table.insert(label, { id .. ' ', guifg = '#434852' })
+      --          end
+      --        end
       --
-      --       if #label > 0 then
-      --         table.insert(label, 1, { '󰛢 ', guifg = '#CA8BFF' })
-      --       end
-      --       return label
-      --     end
-      --     local harpoon_items = get_harpoon_items()
-      --     for _, item in ipairs(harpoon_items) do
-      --       table.insert(res, item)
-      --     end
+      --        if #label > 0 then
+      --          table.insert(label, 1, { '󰛢 ', guifg = '#CA8BFF' })
+      --        end
+      --        return label
+      --      end
+      --      local harpoon_items = get_harpoon_items()
+      --      for _, item in ipairs(harpoon_items) do
+      --        table.insert(res, item)
+      --      end
       --
-      --     -- if ft_icon then
-      --     --   table.insert(res, ' ')
-      --     --   -- table.insert(res, { '', guifg = '#1A1A1A' })
-      --     --   table.insert(res, { ' ', ' ', guibg = '#1A1A1A', guifg = helpers.contrast_color '#1A1A1A' })
-      --     --   table.insert(res, { '', guifg = '#1A1A1A' })
-      --     -- end
+      --      -- if ft_icon then
+      --      --    table.insert(res, ' ')
+      --      --    -- table.insert(res, { '', guifg = '#1A1A1A' })
+      --      --    table.insert(res, { ' ', ' ', guibg = '#1A1A1A', guifg = helpers.contrast_color '#1A1A1A' })
+      --      --    table.insert(res, { '', guifg = '#1A1A1A' })
+      --      -- end
       --
-      --     return res
-      --   end,
+      --      return res
+      --    end,
     }
 
-    -- Aqui está o segredo: já desabilita o incline se incline_active for false
     if not incline_active then
       require('incline').disable()
     end
